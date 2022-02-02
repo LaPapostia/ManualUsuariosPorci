295d723440dcebb2984108b45b754332e866f3c1
Juan Esteban Sepulveda Arboleda <jsepulveda@porcicarnes.com>
2022-02-02 08:28:17 -0500

    Primera Subida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NovedadesComponent } from './Modulos/novedades/novedades.component';
+import {ManualUsuarioComponent } from './Modulos/manual-usuario/manual-usuario.component'
+import { FooterComponent } from './Partials/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: NovedadesComponent,
+  },
+  {
+    path: 'manualUsuario',
+    component: ManualUsuarioComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Partials/footer/footer.component';
+import { HeaderComponent } from './Partials/header/header.component';
+import { NovedadesComponent } from './Modulos/novedades/novedades.component';
+import { ManualUsuarioComponent } from './Modulos/manual-usuario/manual-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NovedadesComponent,
+    ManualUsuarioComponent
   ],
   imports: [
     BrowserModule,
